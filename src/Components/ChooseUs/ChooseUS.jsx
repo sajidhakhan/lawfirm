135052c8c9b39e2d91bdf16668913b91c0288eb1
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-07-22 23:50:24 +0530

    chooseUs sec

--- src/Components/ChooseUs/ChooseUS.jsx
+++ src/Components/ChooseUs/ChooseUS.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import styles from './ChooseUs.module.css'
+import Card from '@mui/material/Card';
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { cards } from "../../constants";
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
 
 export default function ChooseUS() {
   return (
-    <div className={styles.ChooseUSsection}>
+    <>
+      <div className={styles.ChooseUSsection}>
         <h1>Why Choose us?</h1>
+      </div>
+      <div className={styles.cardSection}>
+      <Grid container spacing={1}> {/* Add spacing between cards */}
+        {cards.map((card, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}> {/* Adjust grid sizes as needed */}
+            <Card className={styles.card} sx={{ width: 359, height: 377  }}>
+              <CardMedia component="img"  image={card.image} alt={card.title} sx={{
+                  width: 101,
+                  height: 101,
+                 
+                }} />
+              <CardContent>
+                <h5 className={styles.title}>
+                  {card.title}
+                </h5>
+                <p className={styles.description}>
+                  {card.description}
+                </p>
+              </CardContent>
+              <Button size="small" href={card.readMoreLink}>
+                Read More
+              </Button>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
+      
+    </>
+   
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import cardImg from '../assests/cardImg.png';
+
 export const navLinks = [
     {
        id: "home",
@@ -16,3 +18,25 @@ export const navLinks = [
        title: "About Us",
      },
    ];
+   const cards = [
+    {
+      title: '98% Success Rate',
+      description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      image: cardImg,
+      readMoreLink: 'Read More'
+    },
+    {
+      title: '100% Success Rate',
+      description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      image: cardImg,
+      readMoreLink: 'Read More'
+    },
+    {
+      title: '100% Success Rate',
+      description: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+      image: cardImg,
+      readMoreLink: 'Read More'
+    },
+  ];
+
+  export { cards };
