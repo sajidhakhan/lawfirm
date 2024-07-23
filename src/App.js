166fe2bf0cf1ddfdb7ee7b713b327e2cdafb59cb
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-07-23 17:31:53 +0530

    ClientTestimonials section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ChooseUS from "./Components/ChooseUs/ChooseUS";
 import HomePage from "./Components/HomePage/HomePage";
 import Intro from "./Components/Intro/Intro";
 import Practices from "./Components/Practices/Practices";
+import ClientTestimonials from './Components/ClientTestimonials/ClientTestimonials';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
    <Intro />
    <ChooseUS />
    <Practices />
+   <ClientTestimonials />
    </>
   );
 }
---
+++ src/Components/ClientTestimonials/ClientTestimonials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './ClientTestimonials.module.css';
+
+export default function ClientTestimonials() {
+  return (
+    <>
+     <div className= {styles.ClientContainer}>
+        <div className={styles.leftSection}>
+                <h1>What says our</h1>
+                <h1>happy Clients</h1>
+                
+        </div>
+        
+     </div>
+    </>
+    
+  )
+}
