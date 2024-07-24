0f8c34d4878cb68966b8f33e0cdd84e3f3ec152a
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-07-24 18:51:38 +0530

    newsLetter created

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,8 +4,15 @@ import styles from './NewsLetter.module.css';
 export default function NewsLetter() {
   return (
     <div className= {styles.NewsLetterCont}>
-        <div className={styles.heading}>
-          <h1>Subscribe Our Newsletter</h1>
+        <div className = {styles.NewsLetterSection}>
+            <div className={styles.heading}>
+            <h1>Subscribe Our Newsletter</h1>
+            </div>
+            <div className={styles.inputsContainer}>
+                <input type="text" placeholder="Name :" className={styles.roundedInput}></input>
+                <input type="text" placeholder="Enter your email address" ></input>
+                <button> SEND </button>
+            </div>
         </div>
     </div>
   )
